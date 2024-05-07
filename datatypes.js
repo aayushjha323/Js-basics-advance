040d3c9240d3979d112d38b7d5040a75fc798411
aayushjha323 <jhaaayush323@gmail.com>
2024-05-07 16:08:09 +0530

    first commit

---
+++ datatypes.js
@@ -0,0 +1,5 @@
+let number=""
+let name= "aayush"
+//let value=null
+let changeName=Boolean(name)
+console.table([number,typeof name,typeof value,changeName])
---
+++ test.js
@@ -0,0 +1,9 @@
+const classRoll="1002"
+let classStrength="Too much"
+var classBoys="disturbing"
+
+classStrength="only 100"
+classBoys="funny"
+classIteam="IT"
+
+console.table([classBoys, classRoll,classStrength,classIteam])
