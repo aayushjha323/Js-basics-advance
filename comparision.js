73a7fcff1e0cdca3d69ba4df9efccaafbc1d4a5e
aayushjha323 <jhaaayush323@gmail.com>
2024-05-16 04:29:53 +0530

    change

---
+++ comparision.js
@@ -0,0 +1,27 @@
+
+str1="get"
+str2="lost"
+str3=str1+str2
+console.log(str3);
+
+
+// console.log("1"+2);
+// console.log(1+"2")
+// console.log("3"+2-1)
+
+console.log(+true);
+gameCod=7000
+++gameCod
+console.log(gameCod);
+
+
+// console.log(2>3);
+// console.log(5>2);
+
+// console.log("3">4);
+
+
+console.log(null>0);
+console.log(null<0);
+console.log(null>=0);
+console.log(null<=0);
--- datatypes.js
+++ datatypes.js
@@ -1,5 +1,10 @@
-let number=""
-let name= "aayush"
+let number=9
+let name= ""
 //let value=null
 let changeName=Boolean(name)
 console.table([number,typeof name,typeof value,changeName])
+
+
+let value=3
+let posOfValue = +value
+console.log(posOfValue);
